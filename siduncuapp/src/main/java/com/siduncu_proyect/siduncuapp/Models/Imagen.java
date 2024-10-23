0d26eaf595d62a060f605a03bc666ccec53cb953
Lucas Martin Carballo <lucascarballo8819@gmail.com>
2024-10-23 12:06:16 -0300

    agrega imagen

---
+++ siduncuapp/src/main/java/com/siduncu_proyect/siduncuapp/Models/Imagen.java
@@ -0,0 +1,29 @@
+
+package com.siduncu_proyect.siduncuapp.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author siduncuNotebook
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Imagen {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String url;
+}
--- siduncuapp/src/main/java/com/siduncu_proyect/siduncuapp/Models/Noticia.java
+++ siduncuapp/src/main/java/com/siduncu_proyect/siduncuapp/Models/Noticia.java
@@ -26,6 +26,7 @@ public class Noticia {
     @ManyToOne
     private Categoria categoria;
 
-
+    @OneToOne
+    private Imagen imagenUrl;
 
 }
