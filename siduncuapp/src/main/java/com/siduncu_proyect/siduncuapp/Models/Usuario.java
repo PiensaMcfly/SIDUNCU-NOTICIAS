d2b2bcec2f8a3f99225f17168776f97ed3b12153
Lucas Martin Carballo <lucascarballo8819@gmail.com>
2025-02-21 22:21:18 -0300

    become rebase to before SS

--- siduncuapp/src/main/java/com/siduncu_proyect/siduncuapp/Models/Usuario.java
+++ siduncuapp/src/main/java/com/siduncu_proyect/siduncuapp/Models/Usuario.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.userdetails.User;
 
 @Setter
 @AllArgsConstructor
@@ -18,15 +17,19 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String nombreUsuario;
-    private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String email;
 
     @Enumerated(EnumType.STRING)
     private Rol rol;
 
     public Usuario orElseThrow(Object o) {
-
         return null;
     }
 }
